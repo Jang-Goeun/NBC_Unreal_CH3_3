d552de7ce66aa5adb0cf8b9bd6e19e031b6ae2d7
goeun441 <120786130+Jang-Goeun@users.noreply.github.com>
2026-06-16 11:28:39 +0900

    feat: 프로젝트 생성 및 에셋 추가

---
+++ Source/NBC_Unreal_CH3_3/NBC_Unreal_CH3_3.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "NBC_Unreal_CH3_3.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, NBC_Unreal_CH3_3, "NBC_Unreal_CH3_3" );
